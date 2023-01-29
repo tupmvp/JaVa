72de187ec45888fd80c6feceb8d5e3af0c3e02dc
tupmvp <tupm.vp@gmail.com>
2023-01-29 15:30:13 +0700

    Vi Du Kieu Du Lieu

---
+++ ViDu.java
@@ -0,0 +1,15 @@
+public class ViDu {
+	public static void main(String[] args) {
+		String tenSach = "Lap trinh Java";
+		int namXB = 2023;
+		double giaTien = 1555.2;
+		boolean conSach = true;
+		char maKho = 'M';
+		
+		System.out.println("Ten Sach: "+tenSach);
+		System.out.println("Nam Xuat Ban: "+namXB);
+		System.out.println("Gia Tien: "+giaTien);
+		System.out.println("Con Sach: "+conSach);
+		System.out.println("Ma Kho: "+maKho);
+	}
+}
